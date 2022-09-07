ca9a6b4b178ab249c3c93fd89d55d857227032d1
Jon Toshmatov <info@myitedu.us>
2022-09-07 11:35:28 -0400

    saving Sarvars work from my local to remote

--- sayfiddinovsarvar/september7.php
+++ sayfiddinovsarvar/september7.php
@@ -1,3 +1,38 @@
-Hammaga Salom!
-sdfsdf
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to My World!</title>
+</head>
+<body>
+
+
+<strong>THIS IS A STRONG TEXT</strong>
+<p>This is a regular text</p>
+
+<!-- This is a HTML comment -->
+
+<h1>Greetings From Uzbekistan!</h1>
+<hr>
+<img title="This is a photo of Miami" width="30%" src="https://i.natgeofe.com/n/5de6e34a-d550-4358-b7ef-4d79a09c680e/aerial-beach-miami-florida_16x9.jpg" alt="Photo of Miami">
+<hr>
+<p>
+    Hello Everyone, My name is Jon Toshmatov and I live in New York.
+</p>
+<p>
+    In my free time I play tennis and walk in the park. I have a dog and his name is Puppyusik. <br> I want to go to Spain to watch El Classico game between Real Madrid and Barselona
+</p>
+
+<a target="_blank" title="Click to open Google" href="http://google.com">Google</a>
+
+<img width="100%" src="img/nyc.jpeg" alt="Photo of NYC">
+<hr>
+
+
+</body>
+</html>
+
 
