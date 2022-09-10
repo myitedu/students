04158dfe0391f349ba8953e0450501536eb27c6d
Jon Toshmatov <info@myitedu.us>
2022-09-10 09:33:16 -0400

    Class: sept 10, 2022

---
+++ sayfiddinovsarvar/september10.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/september10.css">
+</head>
+<body>
+<h1 class="header1">Welcome to New York!</h1>
+
+<ul class="list-group">
+    <li class="list-group-item active">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Morbi leo risus</li>
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+
+
+<div class="mytable">
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th>Logo</th>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        </thead>
+        <tr>
+            <td><img src="https://bcassetcdn.com/public/blog/wp-content/uploads/2021/11/30150053/Tide_Logo-1024x1024.png" alt="Logo"></td>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td><img src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/2017/April/35-Famous-Circle-Logos/19_400.png" alt="Logo"></td>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+        <tr>
+            <td><img src="https://www.logolynx.com/images/logolynx/c9/c9f0625cb85de72986b043896fd5d951.png" alt="Logo"></td>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+        </tr>
+        <tr>
+            <td><img src="https://images.squarespace-cdn.com/content/v1/5d5d594927fded0001069a58/1566413223255-XUDA9TT48TIO3LRMNJ8N/ccic-logo-rev.png" alt="Logo"></td>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td><img src="https://seeklogo.com/images/C/company-letter-s-circle-logo-29B6561B82-seeklogo.com.png" alt="Logo"></td>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td><img src="https://bcassetcdn.com/public/blog/wp-content/uploads/2021/11/30150053/Tide_Logo-1024x1024.png" alt="Logo"></td>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td><img src="https://bcassetcdn.com/public/blog/wp-content/uploads/2021/11/30150053/Tide_Logo-1024x1024.png" alt="Logo"></td>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td><img src="https://bcassetcdn.com/public/blog/wp-content/uploads/2021/11/30150053/Tide_Logo-1024x1024.png" alt="Logo"></td>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
