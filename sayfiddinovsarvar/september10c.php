c7d05c57ec9648a6eae18a86364628bd258f1de6
Jon Toshmatov <info@myitedu.us>
2022-09-10 10:05:20 -0400

    Class: living room with tv

---
+++ sayfiddinovsarvar/september10c.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sept 10c 2022</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div id="living_room">
+    <iframe id="tv" src="https://www.youtube.com/embed/An70aZjL0YU?controls=0" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+
+<style>
+    #tv{
+        width: 314px;
+        height: 178px;
+        position: relative;
+        left: 570px;
+        top: 244px;
+        border: none;
+    }
+    #living_room{
+        width: 1200px;
+        height: 800px;
+        background-color: blue;
+        margin: auto;
+        background-image: url("https://static01.nyt.com/images/2021/01/03/realestate/05fix09/oakImage-1609705463136-mobileMasterAt3x.jpg");
+        background-size: 100% 100%;
+    }
+</style>
+
+</body>
+</html>
