797cdaa7f47dfb77bde4bf431abc92053468da4e
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-09-03 10:49:38 -0400

    JT is pushing the living_project

---
+++ barcelona/september3b.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/september3b.css">
+</head>
+<body>
+<div id="living_room">
+    <img id="robot" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/cd1c9a80364113.5cdec491b07d9.gif" alt="robot">
+    <iframe id="mytv" src="https://www.youtube.com/embed/dOVSoFq3O6w?si=tOysjSsPq0RXyeGn?&autoplay=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+</body>
+</html>
