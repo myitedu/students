a0fb6102567f423ef9d4b157b58485dbcd9e41da
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-09-07 16:31:00 -0400

    pushing

---
+++ masariddin/september4.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to September 4, 2024</title>
+</head>
+<body>
+
+<div class="living_room">
+    <h6 class="living_room_header">Welcome to living room!</h6>
+</div>
+
+<div class="kitchen">This is my kitchen</div>
+<div class="bedroom">This is where I take a nap</div>
+
+
+<style>
+
+     .living_room{
+         width: 300px;
+         height: 300px;
+         border: 24px solid #b92004;
+         background-color: #79fca5;
+         display: inline-block;
+         text-align: center;
+         border-radius: 8px;
+         font-size: 26px;
+         background-image: url("https://st.hzcdn.com/simgs/97910d6b0407c3d1_14-0485/_.jpg");
+         background-size: cover;
+         color: #fdb8db;
+     }
+
+     .living_room_header{
+         background-color: #00000066;
+         color: gold;
+         padding: 5px;
+     }
+
+
+     .kitchen{
+         width: 300px;
+         height: 300px;
+         border: 1px solid black;
+         background-color: green;
+         display: inline-block;
+         display: none;
+     }
+     .bedroom{
+         width: 300px;
+         height: 300px;
+         border: 1px solid black;
+         background-color: blue;
+         display: inline-block;
+         display: none;
+     }
+</style>
+
+</body>
+</html>
