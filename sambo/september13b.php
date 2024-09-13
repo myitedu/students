b41b5a706e2482c7e4ac042637fa91fdb19635fc
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-09-13 14:43:53 -0400

    JT is saving sambo project mytv and living room

---
+++ sambo/september13b.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Room with TV</title>
+    <link rel="stylesheet" href="css/september13b.css">
+</head>
+<body>
+<div id="living_room">
+    <iframe id="mytv" src="https://www.youtube.com/embed/GYwRWB48woY?si=sz3YIYYJ0tuOQUP9" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+</body>
+</html>
