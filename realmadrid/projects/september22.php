ce511351f7f0a15e333dca8b6733b2a9e07cf907
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-09-22 14:10:43 -0400

    JT is pushing the code:

---
+++ realmadrid/projects/september22.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Sept 22, 2024</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/september22.css">
+</head>
+<body>
+<div class="container">
+
+    <div id="mywebsite">
+        <div id="div1">
+            <img class="logo" src="https://seeklogo.com/images/C/corporate-company-logo-749CEE6ADC-seeklogo.com.png" alt="logo">
+            <button class="btn-buy-now">&nbsp;&nbsp;&nbsp;Buy Now&nbsp;&nbsp;&nbsp;</button>
+        </div>
+        <div id="div2">
+            <a href="#">Home</a>
+            <a href="#">Live Examples</a>
+            <a href="#">Live Demo Blocks</a>
+        </div>
+        <div id="div3">
+            <h1>Travel Agency Website Design</h1>
+            <p>Fill our this form and manager will contact you</p>
+        </div>
+        <div id="div4" class="row">
+            <div class="col">
+                <input type="text" placeholder="one">
+            </div>
+            <div class="col">
+                <input type="text" placeholder="one">
+            </div>
+            <div class="col">
+                <input type="text" placeholder="one">
+            </div>
+            <div class="col">
+                <button class="search">Search</button>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+</body>
+</html>
