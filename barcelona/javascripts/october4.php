b507c8a044bb71bc3e2b8148de52a1bbfaf20b1e
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-10-04 21:12:02 +0500

    saving

---
+++ barcelona/javascripts/october4.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+<input type="text" id="number">
+<input id="change_number" type="range" min="10" max="1000">
+<style>
+    #change_number{
+        width: 300px;
+    }
+</style>
+<script>
+    $(function () {
+       $("#change_number").change(function () {
+         let n = $(this).val();
+         $("#number").val(n);
+       });
+    });
+</script>
+</body>
+</html>
