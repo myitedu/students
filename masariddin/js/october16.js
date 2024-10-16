0915acff0738d911f94e19a85f04cb99429b1013
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-10-16 13:13:01 -0400

    saving

---
+++ masariddin/js/october16.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $("#knopka").click(function () {
+        $("#yashik")
+            .addClass("jon")
+            .html("Hello, my name is <strong>Jon Toshmatov</strong> and I live in NYC");
+    });
+
+
+
+});
