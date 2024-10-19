a66dfb20ee95f149e58f963553ef0cba6eedd3f9
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-10-19 17:48:02 -0400

    saving

---
+++ akhadjon/october18.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Welcome to my online store!</h1>
+<hr>
+<nav>
+    <a href="https://yahoo.com">Yahoo</a> |
+    <a href="https://google.com">Google</a> |
+    <a href="https://instagram.com">Instagram</a> |
+    <a href="https://facebook.com">Facebook</a> |
+</nav>
+<hr>
+
+<h2>About Me</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+    type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+    Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+    Lorem Ipsum.</p>
+
+<h2>My Education</h2>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its
+    layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to
+    using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web
+    page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web
+    sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on
+    purpose (injected humour and the like).</p>
+
+
+</body>
+</html>
