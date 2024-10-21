cea854f654993fe8f36373943f0b4bb0ec86b85c
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-10-21 09:22:39 -0400

    saving

---
+++ realmadrid/october19.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to my projects!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/october19.css">
+</head>
+<body>
+<div id="mybox">
+    <div class="row toprow">
+        <div class="col col-6">
+            <img class="logo" src="https://www.vernoncompany.com/wp-content/uploads/2021/07/VernonLogoplusIcon_4C.png" alt="logo">
+        </div>
+        <div class="col col-6 toprightcol">
+            <a href="#">Features</a>
+            <a href="#">Pricing</a>
+            <a href="#">Testimonials</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </div>
+    <div class="row bottomrow">
+        <div class="col col-6">
+            <div class="bottom_left_box">
+                <h3>Future is here, <br> Start Exploring Now</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus<br>
+                    eius distinctio odit, magni magnam qui ex perferendis vitae!
+                </p>
+                <p>
+                    <button class="btn btn-info">Get Started</button>
+                </p>
+            </div>
+        </div>
+        <div class="col col-6">
+            <img class="man_photo" src="https://d1sfco99flnudn.cloudfront.net/go.frontier.com/images/partials/split-content/hero/business-man-mobile.png" alt="photo">
+        </div>
+    </div>
+</div>
+</body>
+</html>
