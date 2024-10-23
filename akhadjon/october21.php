d47b034cc5a083dae019c459e6af43d7509b98fc
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-10-23 14:43:46 -0400

    saving

---
+++ akhadjon/october21.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/october21.css">
+</head>
+<body>
+<div id="living_room">
+    <iframe id="youtube_video" src="https://www.youtube.com/embed/752zBp4wpfI?si=p0rYgY2bHeTqj7Cv"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+</body>
+</html>
---
+++ akhadjon/october22.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/october22.css">
+</head>
+<body>
+
+<div id="photo_gallery">
+<h1>Welcome to my Photo Gallery!</h1>
+    <hr>
+    <img class="photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSygH8i-Id7sZ3EXPtyNEnZhiMkZa5tjIVKQA&s" alt="dubai">
+    <img class="photo" src="https://cdn.britannica.com/15/189715-050-4310222B/Dubai-United-Arab-Emirates-Burj-Khalifa-top.jpg" alt="dubai">
+    <img class="photo" src="https://cdn.britannica.com/43/134743-050-D0625A44/train-first-Dubai-emirate-rapid-transit-line-kind-Sept-10-2009.jpg" alt="dubai">
+    <img class="photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3q_XcKt-HtQu-AII-c_RlIx6Nyb5p_foRXg&s" alt="dubai">
+    <img class="photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSR4MFWECenUt_IkaqH-S6mi_ttspzw7efZA&s" alt="dubai">
+    <img class="photo" src="https://www.usatoday.com/gcdn/-mm-/b2b05a4ab25f4fca0316459e1c7404c537a89702/c=0-0-1365-768/local/-/media/2021/03/09/USATODAY/usatsports/imageForEntry25-aa1.jpg?width=1365&height=768&fit=crop&format=pjpg&auto=webp" alt="dubai">
+    <img class="photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlzgGrbxuhyTt6wjhIs5WndYLqLs7Iy2CkTQ&s" alt="dubai">
+    <img class="photo" src="https://www.thesmartbusinesstourist.com/content/images/size/w1000/2023/07/dubai-DUBAITG0721-04b6fdc668e649adb5368b3db401651c-1.jpeg" alt="dubai">
+    <img class="photo" src="https://www.datocms-assets.com/32623/1644529563-traveltodubai_new_hero.jpeg?auto=format" alt="dubai">
+
+
+</div>
+
+
+</body>
+</html>
