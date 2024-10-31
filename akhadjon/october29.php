dd616cfeb6c5c2ad2d0b4fe4f97d9e648d7ceb5f
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-10-31 10:06:27 -0400

    saving:

---
+++ akhadjon/october29.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/october29.css">
+
+</head>
+<body>
+
+<div id="business_card">
+    <div class="left_col">
+        <div id="details">
+            <h1>Jon Toshmatov</h1>
+            <hr>
+
+            <table>
+                <tr>
+                    <td><div class="small_circle"><i class="material-icons">local_phone</i></div></td>
+                    <td>+ 1 (347) 576-4951</td>
+                </tr>
+                <tr>
+                    <td><div class="small_circle"><i class="material-icons">email</i></div></td>
+                    <td>info@myitedu.us</td>
+                </tr>
+                <tr>
+                    <td><div class="small_circle"><i class="material-icons">location_on</i></div></td>
+                    <td>Brooklyn, NY USA</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="circle"></div>
+
+    <div class="right_col">
+        <img id="logo" src="https://preview.redd.it/myudtqveqaf51.png?width=243&format=png&auto=webp&s=b43bac6a4506ed5d7731505d310be9d041d4d2c5" alt="logo">
+    </div>
+</div>
+
+
+</body>
+</html>
