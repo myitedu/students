5d87c874ad81464157d8a992029a42e30a1b4414
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-11-13 10:13:27 -0500

    saving

--- codechallenges/november11.php
+++ codechallenges/november11.php
@@ -1,1 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Typewriters</title>
+    <style>
+        #app {
+            border: dashed black 1px;
+            width: 130px;
+            padding-left: 20px;
+            font-weight: bold;
+            background-color: lightgreen;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Example with 'v-if' and 'v-else'</h1>
+
+<p>Try changing the 'typewritersInStock' value in the Vue instance from 'true' to 'false' and run the code again.</p>
+
+<div id="app">
+    <p v-if="typewritersInStock">
+        in stock
+    </p>
+    <p v-else>
+        not in stock
+    </p>
+
+    <button v-bind:class="btnClass" @click="clickMe">CLICK ME</button>
+</div>
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script>
+    const app = Vue.createApp({
+        data() {
+            return {
+                typewritersInStock: false,
+                counter:0,
+                btnClass:''
+            }
+        },
+        methods:{
+            clickMe(){
+                this.counter++;
+                this.btnClass = "newbtn";
+                console.log("Counter: "+this.counter);
+            }
+        }
+    })
+    app.mount('#app')
+</script>
+
+</body>
+</html>
---
+++ practice/november12.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to New York!</title>
+    <link rel="stylesheet" href="css/november12.css">
+</head>
+<body>
+
+
+<div id="boxes">
+    <div class="left_box">Left</div>
+    <div class="middle_box">Middle</div>
+    <div class="right_box">Right</div>
+</div>
+
+</body>
+</html>
