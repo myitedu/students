26092a31cd29793c3d751ded1dece606561fd392
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-11-18 17:31:16 -0500

    saving

---
+++ akhadjon/javascript/js/november18.js
@@ -0,0 +1,11 @@
+alert("Welcome to Shell Gas Station!");
+
+let dob = prompt("Enter your year of birth: ");
+let age = 2024 - dob;
+
+if (age>=18){
+    alert("Here are you, you can buy those products!");
+}else{
+    alert("Sorry, you are too young to purchase those products");
+}
+
