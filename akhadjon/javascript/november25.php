143bc076e1c95af8a1db5a380219f01f9e2fdbb0
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-11-25 16:01:47 -0500

    saving

---
+++ akhadjon/javascript/november25.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minimal & Modern Furniture</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/november25.css">
+</head>
+<body>
+<!-- Hero Section -->
+<div class="hero-section text-center">
+    <div class="container">
+        <h1 class="fw-bold display-4">Minimal & Modern <span class="text-primary">Furniture</span></h1>
+        <p class="text-muted">Shop the furniture collection today. Make your space feel welcome and complete, with our modern minimalist designer furniture.</p>
+        <div class="image-container">
+            <!-- Image cropped using CSS -->
+            <img src="img/website.png" alt="Furniture" class="cropped-image">
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
