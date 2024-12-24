a516b4f388e3d5d8c2b8ff6e02bf18686ba97596
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-12-24 09:42:50 -0500

    saving

---
+++ masariddin/december23.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Content Is King</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 20px;
+        }
+
+        .title {
+            color: #e74c3c;
+            font-size: 24px;
+            font-weight: bold;
+            text-transform: uppercase;
+            margin-bottom: 10px;
+        }
+
+        .divider {
+            border-bottom: 2px solid #ddd;
+            margin-bottom: 20px;
+        }
+
+        .icon-section {
+            display: flex;
+            align-items: flex-start;
+            margin-top: 20px;
+        }
+
+        .icon {
+            font-size: 40px;
+            margin-right: 15px;
+            color: #ccc;
+        }
+
+        .text-content {
+            flex: 1;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <div class="title">Content is King</div>
+    <div class="divider"></div>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dignissim pretium consectetur. Curabitur tempor posuere massa in varius. Pellentesque viverra nibh eu vehicula mattis.
+    </p>
+    <p>
+        Nullam porta facilisis justo, a feugiat dolor sodales sed. Vestibulum neque nisi, elementum tincidunt metus sit amet, egestas bibendum enim. Aenean a blandit elit, vel rutrum mi. Nunc dapibus turpis sed massa aliquet tincidunt.
+    </p>
+
+    <div class="icon-section">
+        <div class="icon">
+            <i class="material-icons">trending_down</i>
+        </div>
+        <div class="text-content">
+            <p>
+                Egestas bibendum enim. Aenean a blandit elit, vel rutrum mi. Nunc dapibus turpis sed Pellentesque id neque ligula. Nullam posuere, ligula a mattis posuere, lacus nisi adipiscing nunc, eu congue justo est ut diam.
+            </p>
+        </div>
+    </div>
+
+    <div class="icon-section">
+        <div class="icon">
+            <i class="material-icons">language</i>
+        </div>
+        <div class="text-content">
+            <p>
+                Nunc dapibus turpis sed Pellentesque id neque ligula. Nullam posuere, ligula a mattis posuere, lacus nisi adipiscing nunc, eu congue justo est ut diam. Nam quam nibh, dignissim non consequat in.
+            </p>
+        </div>
+    </div>
+</div>
+
+
+
+<script src="https://fonts.googleapis.com/icon?family=Material+Icons"></script>
+</body>
+</html>
