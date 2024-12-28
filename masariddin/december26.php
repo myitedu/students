2f93a27c91cc730a4420503c682781641bad554a
Jon Toshmatov <jontoshmatov@yahoo.com>
2024-12-28 11:20:49 -0500

    saving

---
+++ masariddin/december26.php
@@ -0,0 +1,17 @@
+<?php
+###################################################
+ini_set("display_errors",1);
+###################################################
+$password = "abc123ay";
+if (strlen($password)<8){
+    exit("Your password length must be longer than 8 chars long");
+}
+echo "WOHOOOO";
+
+
+
+
+
+
+
+
